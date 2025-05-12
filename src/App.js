85be0edfb97185680c0c9291f2bc4385a1994317
Paras-22 <parasjaat22ji@gmail.com>
2025-05-13 11:40:19 +1200

    Fix some errors

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import './App.css'; // Here I import the CSS file to style the app
 
 function App() {
   return (
-    <div className="App">  {/* Here I create the main container div for the app */}
-      <header className="App-header">  {/* Here I define the header section of the app */}
-        <img src={logo} className="App-logo" alt="logo" /> {/* Here I add the logo image */}
+    <div className="App">
+      {/* Here I create the main container div for the app */}
+      <header className="App-header">
+        {/* Here I define the header section of the app */}
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* Here I add the logo image */}
         <p>
-          Edit <code>src/App.js</code> and save to reload. {/* Here I add text instructing the user to edit the code */}
+          {/* Here I add text instructing the user to edit the code */}
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* Here I style the link using App-link class and set destination */}
         <a
-          className="App-link"  {/* Here I style the link using App-link class */}
-          href="https://reactjs.org"  {/* Here I set the destination of the link */}
-          target="_blank"  {/* Here I ensure the link opens in a new tab */}
-          rel="noopener noreferrer"  {/* Here I add security for opening the link */}
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Learn React {/* Here I add the text for the link */}
+          {/* Here I add the text for the link */}
+          Learn React
         </a>
       </header>
     </div>
   );
 }
 
-export default App;  {/* Here I export the App component for use elsewhere */}
+export default App; // Here I export the App component for use elsewhere
