19bd0593612f3931fb43b87fe2cd3fadcaf62f11
Paras-22 <parasjaat22ji@gmail.com>
2025-05-13 11:38:53 +1200

    Add comments explaining App component structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'; // Here I import the logo image for the app
+import './App.css'; // Here I import the CSS file to style the app
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App">  {/* Here I create the main container div for the app */}
+      <header className="App-header">  {/* Here I define the header section of the app */}
+        <img src={logo} className="App-logo" alt="logo" /> {/* Here I add the logo image */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {/* Here I add text instructing the user to edit the code */}
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="App-link"  {/* Here I style the link using App-link class */}
+          href="https://reactjs.org"  {/* Here I set the destination of the link */}
+          target="_blank"  {/* Here I ensure the link opens in a new tab */}
+          rel="noopener noreferrer"  {/* Here I add security for opening the link */}
         >
-          Learn React
+          Learn React {/* Here I add the text for the link */}
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App;  {/* Here I export the App component for use elsewhere */}
