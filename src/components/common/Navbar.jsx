8f3b875cf18372991204be7f05e37428307d7751
Ssingh2444 <sahibpreetsingh2444@gmail.com>
2025-05-18 22:53:40 +1200

    Add Navbar Link to Feedback Page

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -325,5 +325,15 @@ const Navbar = ({ sidebarOpen, setSidebarOpen }) => {
     </div>
   );
 };
+// In src/components/common/Navbar.jsx - Add to navigation links
+<Link 
+  to="/feedback" 
+  className={`text-indigo-100 hover:text-white font-medium transition-colors flex items-center ${isActive('/feedback') ? 'border-b-2 border-white' : ''}`}
+>
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+  </svg>
+  Feedback
+</Link>
 
 export default Navbar;
