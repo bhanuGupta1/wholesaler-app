4fcb696534b8858f179bb8995534b301e6ca17f6
Paras-22 <parasjaat22ji@gmail.com>
2025-06-01 05:04:31 +1200

    Create AboutUs component with basic structure.

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,13 @@
+// AboutUs.jsx
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to WholesaleHub, your trusted partner in wholesale commerce.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
