7f3c615bdcdea12750aa2c7f07f4381bdd19d24a
Paras-22 <parasjaat22ji@gmail.com>
2025-06-01 05:19:19 +1200

    Set up the basic page strucure.

---
+++ src/pages/Documentation.jsx
@@ -0,0 +1,13 @@
+// Documentation.jsx
+import React from 'react';
+
+const Documentation = () => {
+  return (
+    <div>
+      <h1>Documentation</h1>
+      <p>Welcome to the WholesaleHub documentation.</p>
+    </div>
+  );
+};
+
+export default Documentation;
