bb64f66f6fb3bb1a61ca441409e8026d62d67f27
Paras-22 <parasjaat22ji@gmail.com>
2025-06-16 01:37:13 +1200

    Created two functions to count the products accoding to the certain criteria.

--- src/components/common/AdvancedFilter.jsx
+++ src/components/common/AdvancedFilter.jsx
@@ -14,3 +14,21 @@ const AdvancedFilter = ({
 }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const [activeFiltersCount, setActiveFiltersCount] = useState(0);
+
+  useEffect(() => {
+    const count = Object.values(filters).filter(value => value && value !== '').length + 
+                 (searchTerm ? 1 : 0);
+    setActiveFiltersCount(count);
+  }, [filters, searchTerm]);
+
+  const handleFilterChange = (filterName, value) => {
+    onFilterChange({
+      ...filters,
+      [filterName]: value
+    });
+  };
+
+  const clearAllFilters = () => {
+    onClearFilters();
+    onSearchChange && onSearchChange('');
+  };
