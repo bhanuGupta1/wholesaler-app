b5a88f66c1ec9a1c87a4af1e2d931e9145cb944c
Paras-22 <parasjaat22ji@gmail.com>
2025-06-18 03:04:14 +1200

    Initialize basic UserSettings component structure

---
+++ src/pages/UserSettings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserSettings = () => {
+  return (
+    <div className="max-w-3xl mx-auto p-6">
+      <h1 className="text-3xl font-bold text-indigo-600">Settings</h1>
+      {/* Content will go here */}
+    </div>
+  );
+};
+
+export default UserSettings;
