4a872bc16d2bc7e4e5d6339f8251831d56675ce7
bhanugupta1 <bhanugupta2001@gmail.com>
2025-06-22 03:04:33 +1200

    fixed App.jsx to get rid of layout from admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -368,9 +368,9 @@ function App() {
                 {/* Role-specific dashboard routes with access control */}
                 <Route path="/admin-dashboard" element={
                   <ProtectedRoute requiredRole="admin">
-                    <Layout>
+                    
                       <AdminDashboard />
-                    </Layout>
+                    
                   </ProtectedRoute>
                 } />
 
