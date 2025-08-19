0a5cb2e617e729e8fefe5ac984feb4ca69d2f25d
bhanugupta1 <bhanugupta2001@gmail.com>
2025-08-20 11:45:18 +1200

    Adding ESLINT config and Trigger CI/CD

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+// eslint.config.js
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn"
+    }
+  }
+];
